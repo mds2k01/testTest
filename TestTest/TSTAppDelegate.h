06f8ca28b648ffa1650e61f9942154c9e01011ba
Marcelo Santos <msant37@GEC-LM-C02JJ0NKDTY3.local>
2013-07-01 17:32:18 -0300

    Initial Commit

---
+++ TestTest/TSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TSTAppDelegate.h
+//  TestTest
+//
+//  Created by Marcelo Santos on 7/1/13.
+//  Copyright (c) 2013 Marcelo Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestTest
+//
+//  Created by Marcelo Santos on 7/1/13.
+//  Copyright (c) 2013 Marcelo Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TSTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TSTAppDelegate class]));
+    }
+}
